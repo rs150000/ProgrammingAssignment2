840e5a8a891bd832ee210e48fa1f1564fba33a92
rs150000 <rs150000@users.noreply.github.com>
2015-07-25 09:27:47 -0400

    class assig2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,35 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
-
+## Creates a list of variables used for testing the
+## existence of a pre calculated matrix inverse
+## to be called in a separate function
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y){
+    x<<-y
+    m<<-NULL
+  }
+  get <- function() x
+  setsolve <- function(solve) m <<- solve
+  getsolve <- function() m
+  list(set = set, get = get, setsolve = setsolve, getsolve = getsolve)  
 }
 
-
-## Write a short comment describing this function
-
+## Returns a matrix that is the inverse of 'x'
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## x$getsolve returns a value from the cache function
+  m <- x$getsolve()
+  ## a matrix times its inverse is the identity function
+  ## a matrix times the identity returns the same matrix
+  ## 'if' tests to determine whether the cached matrix
+  ## is actually an inverse of 'x'
+  if(((x %*% m) %*% x) == x) {
+    message("getting cached data")
+    return(m)
+  }
+  data <-x$get()
+  m <- solve(data, ...)
+  x$setsolve(m)
+  m  
 }
